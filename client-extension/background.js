c937861ed528d97fc36fa866dd4befb7dbae7523
fkerr0890 <fredk101a@gmail.com>
2023-09-19 21:52:55 -0500

    Block web requests on localhost and proxy through p2pbackend, redirect to orignal request after backend responds

--- client-extension/background.js
+++ client-extension/background.js
@@ -1,33 +1,64 @@
 let activeTabId = -1;
+let pendingRequests = {};
 const backendPort = browser.runtime.connectNative('p2pbackend');
+
 backendPort.onDisconnect.addListener((p) => {
     console.log('Disconnected');
     if (p.error) {
-      console.log(`Disconnected due to an error: ${p.error.message}`);
+        console.log(`Disconnected due to an error: ${p.error.message}`);
     }
-  });
+});
+
 backendPort.onMessage.addListener(async response => {
     console.log('From nm host:' + response);
     if (typeof response === 'object') {
         console.log(response['ResourceAvailable']);
     }
-    if (response['ResourceAvailable'] === 'index.html') {
-        console.log('Redirecting...');
+    if (!response['ResourceAvailable']) {
+        return;
+    }
+    if (response['ResourceAvailable'] === 'index.html' && Object.keys(pendingRequests).length === 0) {
+        console.log('Redirecting to home...');
         await browser.tabs.update(activeTabId, {url: 'http://localhost/'});
+    }
+    else if (pendingRequests[response['ResourceAvailable']]) {
+        console.log('Redirecting...');
+        pendingRequests[response['ResourceAvailable']]({redirectUrl: 'http://localhost/' + response['ResourceAvailable']});
     }
 });
 
-browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
-    if (changeInfo.status === 'complete') {
-        console.log('Loading complete');
+function inteceptRequestAsync(requestDetails) {
+    const parts = requestDetails.url.split('/');
+    let resourcePath = parts.slice(3, parts.length).join('/');
+    if (resourcePath === '') {
+        resourcePath = 'index.html';
     }
-});
+    if (pendingRequests[resourcePath]) {
+        pendingRequests[resourcePath] = null;
+        return {cancel: false};
+    }
+    console.log('Request intercepted: ' + JSON.stringify(requestDetails));
+    return new Promise((resolve, reject) => {
+        console.log('Sending search request for ' + resourcePath);
+        backendPort.postMessage(`{"SearchRequest": "${resourcePath}"}`);
+        pendingRequests[resourcePath] = resolve;
+        setTimeout(() => {
+            reject('Request timed out');
+        }, 10000);
+    });
+}
+
+browser.webRequest.onBeforeRequest.addListener(inteceptRequestAsync, {urls: ['<all_urls>']}, ['blocking']);
+
+// browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+//     if (changeInfo.status === 'complete') {
+//         console.log('Loading complete');
+//     }
+// });
+
 browser.omnibox.onInputChanged.addListener((_text, addSuggestions) => {
     addSuggestions([{ 'content': 'https://hello.p2p/', 'description': 'Visit "hello" on the p2p network!' }]);
 });
-browser.omnibox.onInputEntered.addListener(async (url, _disposition) => {
-    activeTabId = (await browser.tabs.create({url: '/my_page.html?id=' + url})).id;
-    console.log('Active tab id: ' + activeTabId);
-    console.log('Sending search request...');
-    backendPort.postMessage('{"SearchRequest": "index.html"}');
+browser.omnibox.onInputEntered.addListener(async (_url, _disposition) => {
+    activeTabId = (await browser.tabs.create({url: 'http://localhost/'})).id;
 });
