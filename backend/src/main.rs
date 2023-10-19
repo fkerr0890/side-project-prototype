3b055b040184bec69ed23721bb2548c839acc3c1
fkerr0890 <fredk101a@gmail.com>
2023-10-18 22:48:43 -0500

    Added new channels

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use p2p::{node::{EndpointPair, SearchRequestProcessor, MessageProcessor}, gateway::{self, OutboundGateway, InboundGateway}, peer::peer_ops, http::{self, ServerContext}};
+use p2p::{node::{EndpointPair, SearchRequestProcessor, MessageProcessor, DiscoverPeerProcessor}, gateway::{self, OutboundGateway, InboundGateway}, peer::peer_ops, http::{self, ServerContext}};
 use tokio::{sync::{mpsc, Mutex}, time::sleep, net::UdpSocket};
 use std::{time::Duration, net::{SocketAddrV4, SocketAddr}, env, sync::Arc, panic, process, collections::HashMap};
 
@@ -13,8 +13,10 @@ async fn main() {
     let remote_endpoint_pair = EndpointPair::new(remote_public_endpoint, remote_private_endpoint);
     
     let socket = Arc::new(UdpSocket::bind(my_private_endpoint).await.unwrap());
-    let (in_gateway_tx, srp_rx) = mpsc::unbounded_channel();
-    let (srp_tx, out_gateway_rx) = mpsc::unbounded_channel();
+    let (srm_to_srp, srm_from_gateway) = mpsc::unbounded_channel();
+    let (srm_to_gateway, srm_from_srp) = mpsc::unbounded_channel();
+    let (dpm_to_dpp, dpm_from_gateway) = mpsc::unbounded_channel();
+    let (dpm_to_gateway, dpm_from_dpp) = mpsc::unbounded_channel();
     let (heartbeat_tx, _) = mpsc::unbounded_channel();
     let (to_http_handler, from_srp) = mpsc::unbounded_channel();
 
@@ -24,9 +26,11 @@ async fn main() {
     if args.len() > 3 {
         local_hosts.insert(args[3].to_owned(), SocketAddrV4::new(args[4].parse().unwrap(), args[5].parse().unwrap()));
     }
-    let mut srp = SearchRequestProcessor::new(MessageProcessor::new(my_endpoint_pair, srp_rx, srp_tx), to_http_handler, local_hosts);
+    let mut srp = SearchRequestProcessor::new(MessageProcessor::new(my_endpoint_pair, srm_from_gateway, srm_to_gateway), to_http_handler, local_hosts);
+    let mut dpp = DiscoverPeerProcessor::new(MessageProcessor::new(my_endpoint_pair, dpm_from_gateway, dpm_to_gateway));
 
-    let mut outbound_gateway = OutboundGateway::new(&socket, out_gateway_rx);
+    let mut outbound_srm_gateway = OutboundGateway::new(&socket, srm_from_srp);
+    let mut outbound_dpm_gateway = OutboundGateway::new(&socket, dpm_from_dpp);
 
     let orig_hook = panic::take_hook();
     panic::set_hook(Box::new(move |panic_info| {
@@ -38,12 +42,27 @@ async fn main() {
 
     tokio::spawn(async move {
         loop {
-            let Some(_) = outbound_gateway.send().await else { return };
+            let Some(_) = outbound_srm_gateway.send().await else { return };
+        }
+    });
+
+    tokio::spawn(async move {
+        loop {
+            let Some(_) = outbound_dpm_gateway.send().await else { return };
         }
     });
     
     for _ in 0..225 {
-        let mut inbound_gateway = InboundGateway::new(&socket, &in_gateway_tx);
+        let mut inbound_gateway = InboundGateway::new(&socket, &srm_to_srp);
+        tokio::spawn(async move {
+            loop {
+                let Ok(_) = inbound_gateway.receive().await else { return };
+            }
+        });
+    }
+
+    for _ in 0..2 {
+        let mut inbound_gateway = InboundGateway::new(&socket, &dpm_to_dpp);
         tokio::spawn(async move {
             loop {
                 let Ok(_) = inbound_gateway.receive().await else { return };
@@ -59,12 +78,18 @@ async fn main() {
 
     tokio::spawn(async move {
         loop {
+            let Ok(_) = dpp.receive().await else { return };            
+        }
+    });
+
+    tokio::spawn(async move {
+        loop {
             let Ok(_) = peer_ops::send_heartbeats(my_endpoint_pair).await else { return };
             sleep(Duration::from_secs(29)).await;
         }
     });
 
-    let server_context = ServerContext::new(&in_gateway_tx, Arc::new(Mutex::new(from_srp)));
+    let server_context = ServerContext::new(&srm_to_srp, Arc::new(Mutex::new(from_srp)));
     http::tcp_listen(SocketAddr::from(([127,0,0,1], 80)), server_context).await;
 
 }
