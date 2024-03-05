2a92dba633bd1f4d2de5893afb1bc7e93e7b5d41
fkerr0890 <fredk101a@gmail.com>
2024-03-05 00:17:29 -0600

    Removed hardcoding in extension

--- client-extension/background.js
+++ client-extension/background.js
@@ -1,50 +1,22 @@
-let activeTabId = -1;
-let pendingRequests = {};
-// const backendPort = browser.runtime.connectNative('p2pbackend');
-
-// backendPort.onDisconnect.addListener((p) => {
-//     console.log('Disconnected');
-//     if (p.error) {
-//         console.log(`Disconnected due to an error: ${p.error.message}`);
-//     }
-// });
-
-// backendPort.onMessage.addListener(async response => {
-//     console.log('From nm host:' + response);
-//     if (typeof response === 'object') {
-//         console.log(response['ResourceAvailable']);
-//     }
-//     if (!response['ResourceAvailable']) {
-//         return;
-//     }
-//     if (response['ResourceAvailable'] === 'index.html' && Object.keys(pendingRequests).length === 0) {
-//         console.log('Redirecting to home...');
-//         await browser.tabs.update(activeTabId, {url: 'http://localhost/'});
-//     }
-//     else if (pendingRequests[response['ResourceAvailable']]) {
-//         console.log('Redirecting...');
-//         pendingRequests[response['ResourceAvailable']]();
-//     }
-// });
+let activeApps = new Map();
 
 function inteceptRequestAsync(requestDetails) {
-    if (requestDetails.url.includes('~')) {
+    const parts = requestDetails.url.split('/');
+    if (parts[3][0] == '~') {
+        activeApps.set(requestDetails.tabId, parts[3].slice(1));
+        return {};
+    }
+    const activeApp = activeApps.get(requestDetails.tabId);
+    if (!activeApp) {
         return {};
     }
     console.log('request intercepted');
-    const parts = requestDetails.url.split('/');
-    parts.splice(3, 0, '~example');
+    parts.splice(3, 0, '~' + activeApp);
     console.log('original url: ' + requestDetails.url + ', new url: ' + parts.join('/'))
     return { redirectUrl: parts.join('/') }
 }
 
 browser.webRequest.onBeforeRequest.addListener(inteceptRequestAsync, {urls: ["<all_urls>"]}, ['blocking']);
-
-// browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
-//     if (changeInfo.status === 'complete') {
-//         console.log('Loading complete');
-//     }
-// });
 
 browser.omnibox.onInputChanged.addListener((_text, addSuggestions) => {
     addSuggestions([{ 'content': 'https://hello.p2p/', 'description': 'Visit "hello" on the p2p network!' }]);
